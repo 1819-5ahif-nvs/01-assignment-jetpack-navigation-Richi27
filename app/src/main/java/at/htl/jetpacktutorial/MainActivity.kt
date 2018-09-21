5619df01d4e9892cffdd7bd799fb63f7999c1307
Richard Hoang <richi.hoang@gmx.at>
2018-09-21 14:25:58 +0200

    added jetpack tutorial project

---
+++ app/src/main/java/at/htl/jetpacktutorial/MainActivity.kt
@@ -0,0 +1,27 @@
+package at.htl.jetpacktutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import at.htl.jetpacktutorial.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    lateinit var drawer: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        val host = supportFragmentManager.findFragmentById(R.id.my_fragment) as NavHostFragment? ?:return
+        val navConfig = host.navController
+
+        drawer = findViewById(R.id.container)
+        NavigationUI.setupActionBarWithNavController(this,navConfig,drawer)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(drawer,Navigation.findNavController(this,R.id.my_fragment))
+    }
+}
---
+++ app/src/main/java/at/htl/jetpacktutorial/SecondFragment.kt
@@ -0,0 +1,39 @@
+package at.htl.jetpacktutorial
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nextButton.setOnClickListener{
+            val nameBundle= Bundle()
+            nameBundle.putString("nameArg",nameText.text.toString())
+            Navigation.findNavController(it).navigate(R.id.action_secondFragment_to_thirdFragment,nameBundle)
+        }
+    }
+}
